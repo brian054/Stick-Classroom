334124c2e7b7901365e49d327c54f5b43201cc4b
brian054 <brian.rowell54@gmail.com>
2024-02-05 19:19:20 -0500

    Maybe just do 5-6 levels, something quick, wanna host on a portfolio site. Steam?

---
+++ main.js
@@ -0,0 +1,26 @@
+/*
+
+What we need to figure out
+- Window dim's
+- Player Sprite
+- Controls
+- Collision with desks and walls
+- Teacher AI
+- Levels
+
+Separate Project Idea:
+Hmmmm now I'm thinking 2d tile rpg right - hmmm we 
+could do a more story driven thing there - like an rpg
+where you go through and cheat your way through school - bet.
+    - TODO: 2d TileRPG engine in JS, 2d RPG in MonoGame, pick whichever you like more
+
+
+*/
+
+function update() {
+
+}
+
+function render() {
+
+}

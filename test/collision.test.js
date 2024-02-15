77e03c9964b17bf3c85c4e9f44ff749c832139d9
brian054 <brian.rowell54@gmail.com>
2024-02-14 21:28:16 -0500

    spell probz

--- test/collision.test.js
+++ test/collision.test.js
@@ -12,7 +12,7 @@ describe('AABB_Collision', () => {
     test("Should return error due to undefined parameters", () => {
         // Why the arrow function tho?
         // 'expect' needs a function to invoke so that it can catch any exceptions thrown by AABB().
-        // If we called AABB directly without wrapping it in a function like we do here, the execption
+        // If we called AABB directly without wrapping it in a function like we do here, the exception
         // would be thrown before 'expect' has a chance to catch it, resulting in failed test like before
         expect(() => AABB_Collision(undefined, 0, 10, 10, 5, 5, 10, 10)).toThrow("AABB(): One or more parameters undefined.");
     })
